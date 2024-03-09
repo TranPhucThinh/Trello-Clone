628a26c2dc1a34a67d59173575e82c1e71f848b8
thinhtp <thinhtp@greenglobal.vn>
2024-03-09 23:40:25 +0700

    Feat: add list header

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -0,0 +1,96 @@
+import { toast } from 'sonner'
+import React, { ElementRef, useRef, useState } from 'react'
+import { useEventListener } from 'usehooks-ts'
+
+import { List } from '@prisma/client'
+import { useAction } from '@/hooks/use-action'
+import { updateList } from '@/action/update-list'
+import { FormInput } from '@/components/form/form-input'
+
+interface ListHeaderProps {
+  data: List
+}
+
+const ListHeader = ({ data }: ListHeaderProps) => {
+  const formRef = useRef<ElementRef<'form'>>(null)
+  const inputRef = useRef<ElementRef<'input'>>(null)
+
+  const [title, setTitle] = useState(data.title)
+  const [isEditing, setIsEditing] = useState(false)
+
+  const enableEditing = () => {
+    setIsEditing(true)
+    setTimeout(() => {
+      inputRef.current?.focus()
+      inputRef.current?.select()
+    })
+  }
+
+  const disableEditing = () => {
+    setIsEditing(false)
+  }
+
+  const { execute } = useAction(updateList, {
+    onSuccess: (data) => {
+      toast.success(`Renamed to "${data.title}"`)
+      setTitle(data.title)
+      disableEditing()
+    },
+    onError: (err) => {
+      toast.error(err)
+    },
+  })
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get('title') as string
+    const id = formData.get('id') as string
+    const boardId = formData.get('boardId') as string
+
+    if (title === data.title) {
+      return disableEditing()
+    }
+
+    execute({ title, id, boardId })
+  }
+
+  const onBlur = () => {
+    formRef.current?.requestSubmit()
+  }
+
+  const onKeyDown = (event: KeyboardEvent) => {
+    if (event.key === 'Escape') {
+      formRef.current?.requestSubmit()
+    }
+  }
+
+  useEventListener('keydown', onKeyDown)
+
+  return (
+    <div className="pt-2 px-2 text-sm font-semibold flex justify-between items-start gap-x-2">
+      {isEditing ? (
+        <form ref={formRef} action={onSubmit} className="flex-1 px-[2px]">
+          <input hidden id="id" name="id" value={data.id} />
+          <input hidden id="boardId" name="boardId" value={data.boardId} />
+          <FormInput
+            ref={inputRef}
+            onBlur={onBlur}
+            id="title"
+            placeholder="Enter list title..."
+            defaultValue={title}
+            className="text-sm px-[7px] py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition truncate bg-transparent focus:bg-white"
+          />
+          <button type="button" hidden />
+        </form>
+      ) : (
+        <div
+          onClick={enableEditing}
+          className="w-full text-sm px-2.5 py-1 h-7 font-medium border-transparent"
+        >
+          {title}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default ListHeader
